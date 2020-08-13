e3ccf88f73852e90b2435c8d01b4d359cb0158dc
Vydro <anonyme>
2020-08-13 12:10:25 +0200

    cryptage MD5

--- ENI-Enchere/src/Servlet/ServletConnexion.java
+++ ENI-Enchere/src/Servlet/ServletConnexion.java
@@ -1,6 +1,9 @@
 package Servlet;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -34,6 +37,7 @@ public class ServletConnexion extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String identifiant = (String) request.getParameter("identifiant");
 		String mot_de_passe = (String) request.getParameter("mot_de_passe");
+		mot_de_passe = getMd5(mot_de_passe);
 		
 		UtilisateurManager um = new UtilisateurManager();
 		boolean con = um.connexion(identifiant, mot_de_passe);
@@ -51,5 +55,32 @@ public class ServletConnexion extends HttpServlet {
 		}
 				
 	}
+	
+	public static String getMd5(String input) 
+    { 
+        try { 
+  
+            // Static getInstance method is called with hashing MD5 
+            MessageDigest md = MessageDigest.getInstance("MD5"); 
+  
+            // digest() method is called to calculate message digest 
+            //  of an input digest() return array of byte 
+            byte[] messageDigest = md.digest(input.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            } 
+            return hashtext; 
+        }  
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            throw new RuntimeException(e); 
+        } 
+    }
 
 }
